90fefcb495b8fb0241b20fbdae8ac55e01900ce3
akanksha52 <akankshagangrade27@gmail.com>
2025-09-14 11:03:09 +0530

    Added a new evaluation feature Adjusted R-square

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ def model_normal(df):
     theta=np.linalg.inv(x.T.dot(x)).dot(x.T).dot(y)
     y_pred=x.dot(theta)
     
-    return evaluation(y, y_pred)
+    return evaluation(y, y_pred, x)
 
 
 def model_gradient(df):
@@ -35,27 +35,32 @@ def model_gradient(df):
         gradient=(1/m)*x.T.dot(error)
         theta-=alpha * gradient      
     
-    return evaluation(y, y_pred)
+    return evaluation(y, y_pred, x)
 
 
-def evaluation(y_true, y_pred):
+def evaluation(y_true, y_pred, x):
     mse=np.mean((y_pred-y_true)**2)
     rmse=np.sqrt(mse)
     r2=1-(np.sum((y_true-y_pred)**2)/np.sum((y_true-np.mean(y_true))**2))    
-    return mse, rmse, r2
+    n=len(y_true)
+    n_features=x.shape[1]-1
+    adj_r2=1-(1-r2)*(n-1)/(n-n_features-1)
+    return mse, rmse, r2, adj_r2
 
 
 if __name__=='__main__':
     df=normalise_data.normalise_data('Housing.csv')
-    a, b, c=model_normal(df)
+    a, b, c, d=model_normal(df)
     print("Using normal equations to find the coefficient of linear regression, results are as follow: ")
     print("MSE: ", a)
     print("RMSE: ", b)
     print("R-square:", c)
+    print("Adjusted R-square: ", d)
     print()
     print()
-    a, b, c=model_gradient(df)
+    a, b, c, d=model_gradient(df)
     print("Using gradient to find the coefficient of linear regression, with 10,000 iterations and learning rate set to 0.1, results are as follow: ")
     print("MSE: ", a)
     print("RMSE: ", b)
     print("R-square:", c)
+    print("Adjusted R-square: ", d)
